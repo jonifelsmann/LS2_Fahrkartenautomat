183d66444f61309ea4d8599067c2f00e3f57b209
jonifelsmann <felsmannjonathan@gmail.com>
2022-10-19 10:21:05 +0200

    test plus

--- LS2_Fahrkartenautomat/src/Fahrkartenautomat.java
+++ LS2_Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -12,11 +12,11 @@ class Fahrkartenautomat {
 		double nochZuZahlen;
 		int test;
 
-		// 1
+		// Geldbetrag eingeben
 		System.out.print("Zu zahlender Betrag (Euro): ");
 		zuZahlenderBetrag = tastatur.nextDouble();
 
-		// 2
+		// Geldeinwurf
 		eingezahlterGesamtbetrag = 0.0;
 		nochZuZahlen = 0.0;
 		while (eingezahlterGesamtbetrag < zuZahlenderBetrag) {
@@ -27,7 +27,7 @@ class Fahrkartenautomat {
 			eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + eingeworfeneMuenze;
 		}
 		
-		// 3
+		// Fahrscheinausgabe
 		System.out.println("\nFahrschein wird ausgegeben");
 		for (int i = 0; i < 8; i++) {
 			System.out.print("=");
@@ -40,7 +40,7 @@ class Fahrkartenautomat {
 		}
 		System.out.println("\n\n");
 		
-		// 4
+		// Rückgeldberechnung und -ausgabe
 		rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
 		if (rueckgabebetrag > 0.0) {
 			System.out.println("Der Rückgabebetrag in Höhe von " + rueckgabebetrag + " Euro");
